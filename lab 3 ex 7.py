ee1f9d61d2c679ef80d137a7204bc5383f8f3907
shizakhan <45127803+shizakhan@users.noreply.github.com>
2019-01-26 23:44:42 +0500

    Add files via upload

---
+++ lab 3 ex 7.py
@@ -0,0 +1,12 @@
+print('Shiza Khan','18B-130-CS')
+print('lab 3','exercise 7')
+print('\n')
+import math
+height = 100
+print('stone is dropped from a height of:',height,'feet')
+acceleration = 32
+print('acceleration due to gravity is:',acceleration,'ft/s**2')
+ini_velocity = 0
+print('the initial velocity is:',ini_velocity,'ft/s')
+final_velocity = 2*acceleration*height + ini_velocity**2
+print('stone hit the ground with a velocity:',math.sqrt(final_velocity),'ft/s')
---
+++ lab 3 ex6.py
@@ -0,0 +1,11 @@
+print('Shiza Khan','18B-130-CS')
+print('lab 3','exercise 6')
+print('\n')
+initial_velocity = 50
+print('The initial velocity is:',initial_velocity,'miles/hr')
+acceleration = 10
+print('The acceleration is:',acceleration,'miles/hr**2')
+time = 2
+print('The time travels by car is:',time,'hours')
+final_velocity = (initial_velocity + acceleration * time)
+print('The car travels by:',final_velocity,'miles/hr')
